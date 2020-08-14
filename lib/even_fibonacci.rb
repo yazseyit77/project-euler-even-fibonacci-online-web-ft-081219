ee0a4fbd409ebcd688d3cd8000e8bc299cdb3b7d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 19:01:49 +0000

    Automatically backed up by Learn

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  fib_nums = []
+  sum = 0
+  prevN = 0
+  nextN = 0
+  while sum < limit
+    sum = prevN + nextN
+    break if sum > limit
+    
