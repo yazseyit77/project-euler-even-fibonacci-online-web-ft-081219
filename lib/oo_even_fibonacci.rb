78788b228f726816cf64a44808cc9dc30ea8a211
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 19:04:50 +0000

    Automatically backed up by Learn

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -8,4 +8,17 @@ class even_fibonacci_sum
     @sum = sum
   end
 
+  def sum
+    fib_nums = []
+    sum = 0
+    prevN = 0
+    nextN = 1
+    while sum < @limit
+      sum = prevN + nextN
+      break if sum > @limit
+      fib_nums << sum if sum.even?
+      prevN = nextN
+      nextN sum
+    end
+    fib_nums.reduce(0, :+)
 end
